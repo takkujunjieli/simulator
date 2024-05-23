f12b59906fb2414ba3695ee0d063c43f88055406
(Takku) Junjie Li <48248589+takkujunjieli@users.noreply.github.com>
2024-05-23 15:07:33 -0700

    Delete BJMR directory

--- BJMR/server/utils/confirmationToken.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-const generateConfirmationToken = (userId) => {
-  const currentTime = Date.now().toString();
-  const tokenData = `${currentTime}${userId}`;
-  return crypto.createHash("sha256").update(tokenData).digest("hex");
-};
-
-module.exports = generateConfirmationToken;
